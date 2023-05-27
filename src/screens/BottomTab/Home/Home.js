9d7a32be35d001faf974f13d887a2b6d2b207067
SummayyaAshfaq <summayyaashfaq@mtechub.org>
2023-05-27 12:29:47 +0500

    G1-73 signIn Fix

--- src/screens/BottomTab/Home/Home.js
+++ src/screens/BottomTab/Home/Home.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useCallback } from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 import {SafeAreaView, ScrollView, View, Text, FlatList} from 'react-native';
 
 ///////////////app components////////////////
@@ -26,7 +26,7 @@ import InstaStory from 'react-native-insta-story';
 
 /////////////////redux///////////
 import {useDispatch} from 'react-redux';
-import { setItemDetail } from '../../../redux/ItemSlice';
+import {setItemDetail} from '../../../redux/ItemSlice';
 
 const data = [
   {
@@ -78,84 +78,86 @@ const Home = ({navigation}) => {
   const dispatch = useDispatch();
 
   //////////loader state/////
-  const [isLoading,setLoading]=useState(false)
-  const[page,setPage]=useState(1)
-  const[refreshing,setRefreshing]=useState(false)
+  const [isLoading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [refreshing, setRefreshing] = useState(false);
 
-    /////////////Get Notification/////////////
-    const [dashboard_stories, setDashboardStories] = useState([]);
-    const GetDashboardStories =useCallback( async () => {
-      var user_id = await AsyncStorage.getItem('User_id');
-      var token = await AsyncStorage.getItem('JWT_Token');
-      var headers = {
-        Authorization: `Bearer ${JSON.parse(token)}`,
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      };
-      await fetch( BASE_URL + 'ads/get_ads_by_screen', {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify({
-          screen_id:"1"
-        }),
-      })
-        .then(response => response.json())
-        .then(async response => {
-          console.log('response here in stories : ', response);
-         //setDashboardStories(response.result)
-          const outputArray = response.result.map(item => ({
-            user_id: item.id+1,
-            user_image: item.image ? `${BASE_URL+item.image}` : 'path_to_placeholder_image',
-            user_name: 'Ad name',
-            stories: [
-              {
-                story_id: item.id,
-                story_image: item.image ? `${BASE_URL+item.image}` : 'path_to_placeholder_image',
-                swipeText: 'Custom swipe text for this story',
-                onPress: () => console.log(`story ${item.id} swiped`),
-              }
-            ]
-          }));
-          
-          console.log( "heree====",outputArray);
-          setDashboardStories([...dashboard_stories,outputArray]);
-          console.log('response after set in stories : ', dashboard_stories);
-        })
-        .catch(error => {
-          console.log('Error  : ', error);
-        });
-    }, [dashboard_stories]);
+  /////////////Get Notification/////////////
+  const [dashboard_stories, setDashboardStories] = useState([]);
+  const GetDashboardStories = useCallback(async () => {
+    var user_id = await AsyncStorage.getItem('User_id');
+    var token = await AsyncStorage.getItem('JWT_Token');
+    var headers = {
+      Authorization: `Bearer ${JSON.parse(token)}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    };
+    await fetch(BASE_URL + 'ads/get_ads_by_screen', {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify({
+        screen_id: '1',
+      }),
+    })
+      .then(response => response.json())
+      .then(async response => {
+        console.log('response here in stories : ', response);
+        //setDashboardStories(response.result)
+        const outputArray = response.result.map(item => ({
+          user_id: item.id + 1,
+          user_image: item.image
+            ? `${BASE_URL + item.image}`
+            : 'path_to_placeholder_image',
+          user_name: 'Ad name',
+          stories: [
+            {
+              story_id: item.id,
+              story_image: item.image
+                ? `${BASE_URL + item.image}`
+                : 'path_to_placeholder_image',
+              swipeText: 'Custom swipe text for this story',
+              onPress: () => console.log(`story ${item.id} swiped`),
+            },
+          ],
+        }));
 
-    const GetDashboardStories11 = useCallback( async () => {
-      var user_id = await AsyncStorage.getItem('User_id');
-      axios({
-        method: 'POST',
-        url: BASE_URL + 'ads/get_ads_by_screen',
-        body: {
-          screen_id:"1"
-        },
+        console.log('heree====', outputArray);
+        setDashboardStories([...dashboard_stories, outputArray]);
+        console.log('response after set in stories : ', dashboard_stories);
       })
-        .then(async function (response) {
-          //console.log('here stories data',response.data)
-  if(response.data.status === true)
-  {
-    setDashboardStories(response.data.result);
-    setLoading(false)
-  }else{
-    <NoDataFound title={'No data here'}/>
-    setLoading(false)
-  }
-  
-        })
-        .catch(function (error) {
-          console.log('error', error);
-        });
-      },[dashboard_items])
+      .catch(error => {
+        console.log('Error  : ', error);
+      });
+  }, [dashboard_stories]);
+
+  const GetDashboardStories11 = useCallback(async () => {
+    var user_id = await AsyncStorage.getItem('User_id');
+    axios({
+      method: 'POST',
+      url: BASE_URL + 'ads/get_ads_by_screen',
+      body: {
+        screen_id: '1',
+      },
+    })
+      .then(async function (response) {
+        //console.log('here stories data',response.data)
+        if (response.data.status === true) {
+          setDashboardStories(response.data.result);
+          setLoading(false);
+        } else {
+          <NoDataFound title={'No data here'} />;
+          setLoading(false);
+        }
+      })
+      .catch(function (error) {
+        console.log('error', error);
+      });
+  }, [dashboard_items]);
 
   /////////////Get Notification/////////////
   const [dashboard_items, setDashboardItems] = useState('');
 
-  const GetDashboardItems = useCallback( async () => {
+  const GetDashboardItems = useCallback(async () => {
     var user_id = await AsyncStorage.getItem('User_id');
     axios({
       method: 'POST',
@@ -163,65 +165,73 @@ const Home = ({navigation}) => {
       body: {},
     })
       .then(async function (response) {
-        console.log("here iem data,",response.data.result)
-if(response.data.status === true)
-{
-
-  // setDashboardItems(page === 1 ? dashboard_items : [ ...dashboard_items,response.data.result],) 
-  setLoading(false)
-  // setRefreshing(false)
-  setDashboardItems(response.data.result);
-
-}else{
-  <NoDataFound title={'No data here'}/>
-  setLoading(false)
-  setRefreshing(false)
-}
-
+        console.log('here iem data,', response.data.result);
+        if (response.data.status === true) {
+          // setDashboardItems(page === 1 ? dashboard_items : [ ...dashboard_items,response.data.result],)
+          setLoading(false);
+          // setRefreshing(false)
+          setDashboardItems(response.data.result);
+        } else {
+          <NoDataFound title={'No data here'} />;
+          setLoading(false);
+          setRefreshing(false);
+        }
       })
       .catch(function (error) {
         console.log('error', error);
       });
-    },[dashboard_items])
+  }, [dashboard_items]);
 
-   const  handleEndReached = () => {
-      if (!isLoading) {
-        // Increment the page count and fetch the next page of data
-        setPage( page + 1 )
-        GetDashboardItems();
-      }
-    };
-   const onRefresh = () => {
-    setRefreshing(true)
+  const handleEndReached = () => {
+    if (!isLoading) {
+      // Increment the page count and fetch the next page of data
+      setPage(page + 1);
+      GetDashboardItems();
+    }
+  };
+  const onRefresh = () => {
+    setRefreshing(true);
     GetDashboardItems();
-    };
+  };
   useEffect(() => {
-    setLoading(true)
- GetDashboardItems();
-    GetDashboardStories()
+    setLoading(true);
+    GetDashboardItems();
+    GetDashboardStories();
   }, []);
 
   const renderItem = ({item}) => {
     return (
       <DashboardCard
-        image={ BASE_URL + item.images[0] }
+        image={BASE_URL + item.images[0]}
         maintext={item.name}
         subtext={item.location}
         price={item.price}
         images_array_length={item.images.length}
         onpress={() => {
-          dispatch(setItemDetail({id:item.id, navplace:'Home'}));
+          dispatch(setItemDetail({id: item.id, navplace: 'Home'}));
           navigation.navigate('ItemDetails', {
             Item_id: item.id,
           });
-
         }}
       />
     );
   };
+  const storyrenderItem = ({item}) => {
+    return (
+      <Lightbox navigator={navigator}>
+        <Image
+          style={{height: 300}}
+          source={{
+            uri: 'http://knittingisawesome.com/wp-content/uploads/2012/12/cat-wearing-a-reindeer-hat1.jpg',
+          }}
+        />
+        <Text style={{color: 'black'}}>Visit here</Text>
+      </Lightbox>
+    );
+  };
   return (
     <SafeAreaView style={styles.container}>
-        <Loader isLoading={isLoading} />
+      <Loader isLoading={isLoading} />
       <ScrollView
         showsHorizontalScrollIndicator={false}
         showsVerticalScrollIndicator={false}>
@@ -232,7 +242,19 @@ if(response.data.status === true)
             navigation.toggleDrawer();
           }}
         />
-        <InstaStory
+        <FlatList
+          data={dashboard_items}
+          horizontal={true}
+          renderItem={renderItem}
+          initialNumToRender={10}
+          maxToRenderPerBatch={10}
+          removeClippedSubviews={true} // Unmount components when outside of window
+          updateCellsBatchingPeriod={100} // Increase time between renders
+          windowSize={7} // Reduce the window size
+          keyExtractor={(item, index) => index}
+          scrollEnabled={false}
+        />
+        {/* <InstaStory
           data={dashboard_stories}
           duration={10}
           onStart={item => console.log(item)}
@@ -245,7 +267,7 @@ if(response.data.status === true)
             </View>
           }
           style={{marginTop: 16}}
-        />
+        /> */}
         <View style={styles.bottomlineview}></View>
         <FlatList
           data={dashboard_items}
